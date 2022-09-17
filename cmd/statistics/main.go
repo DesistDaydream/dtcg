2f0723bd21712e748ec5eb00dfd3701f1f55751a
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-09-17 23:05:57 +0800

    添加统计每个等级的各种 DP 数量的逻辑

--- cmd/statistics/main.go
+++ cmd/statistics/main.go
@@ -1,38 +1,80 @@
 package main
 
 import (
+	"fmt"
+	"sort"
+	"strconv"
+
 	"github.com/DesistDaydream/dtcg/cards"
-	"github.com/DesistDaydream/dtcg/pkg/sdk/cn/models"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 )
 
-func RareDegreeStatistics(cardGroup string, cardDescs []models.CardDesc) {
-	var (
-		原画  int
-		sec int
-		sr  int
-	)
+// 统计每个卡盒中各种稀有度的数量以及异画数量
+func RareDegreeStatistics(cardGroups []string) {
+	for _, cardGroup := range cardGroups {
+		var (
+			原画  int
+			sec int
+			sr  int
+		)
 
-	for _, cardDesc := range cardDescs {
-		if cardDesc.ParallCard == "1" {
-			原画++
-			if cardDesc.RareDegree == "隐藏稀有（SEC）" {
-				sec++
-			}
-			if cardDesc.RareDegree == "超稀有（SR）" {
-				sr++
+		cardsDesc, err := cards.GetCardDesc(cardGroup)
+		if err != nil {
+			logrus.Fatalln(err)
+		}
+
+		for _, cardDesc := range cardsDesc {
+			if cardDesc.ParallCard == "1" {
+				原画++
+				if cardDesc.RareDegree == "隐藏稀有（SEC）" {
+					sec++
+				}
+				if cardDesc.RareDegree == "超稀有（SR）" {
+					sr++
+				}
 			}
 		}
+
+		logrus.WithFields(logrus.Fields{
+			"数量":  len(cardsDesc),
+			"原画":  原画,
+			"异画":  len(cardsDesc) - 原画,
+			"SEC": sec,
+			"SR":  sr,
+		}).Infof("【%v】卡包统计", cardGroup)
+	}
+}
+
+func DPStatistics(cardGroups []string) {
+	var alldps []int
+	for _, cardGroup := range cardGroups {
+		cardsDesc, err := cards.GetCardDesc(cardGroup)
+		if err != nil {
+			logrus.Fatalln(err)
+		}
+
+		var dps []int
+
+		for _, cardDesc := range cardsDesc {
+			if cardDesc.ParallCard == "1" {
+				if cardDesc.CardLevel == "Lv.3" {
+					dp, _ := strconv.Atoi(cardDesc.Dp)
+					dps = append(dps, dp)
+				}
+			}
+		}
+
+		alldps = append(alldps, dps...)
 	}
 
-	logrus.WithFields(logrus.Fields{
-		"数量":  len(cardDescs),
-		"原画":  原画,
-		"异画":  len(cardDescs) - 原画,
-		"SEC": sec,
-		"SR":  sr,
-	}).Infof("【%v】卡包统计", cardGroup)
+	sort.Ints(alldps[:])
+	// 统计 alldps 中的重复元素的个数
+
+	// fmt.Println(alldps)
+	for _, v := range alldps {
+		fmt.Println(v)
+	}
 }
 
 type Flags struct {
@@ -56,19 +98,13 @@ func main() {
 		logrus.Fatalln(err)
 	}
 
-	for _, cardGroup := range cardGroups {
-		cardDescs, err := cards.GetCardDesc(cardGroup)
-		if err != nil {
-			logrus.Fatalln(err)
-		}
-
-		switch flags.StatisticsInfo {
-		case "rare-degree":
-			// 统计卡盒稀有度信息
-			RareDegreeStatistics(cardGroup, cardDescs)
-		default:
-			logrus.Fatalln("指定要统计的信息")
-		}
+	switch flags.StatisticsInfo {
+	case "rare-degree":
+		RareDegreeStatistics(cardGroups)
+	case "dp":
+		DPStatistics(cardGroups)
+	default:
+		logrus.Fatalln("指定要统计的信息")
 
 	}
 }
