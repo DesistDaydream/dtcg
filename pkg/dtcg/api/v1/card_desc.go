b16ff8a0357e8b7c527302328c62451a982f6792
DesistDaydream <373406000@qq.com>
2022-11-14 17:52:02 +0800

    card desc 接口改为根据条件获取卡牌描述

--- pkg/dtcg/api/v1/card_desc.go
+++ pkg/dtcg/api/v1/card_desc.go
@@ -3,6 +3,8 @@ package v1
 import (
 	"net/http"
 
+	dbmodels "github.com/DesistDaydream/dtcg/internal/database/models"
+
 	"github.com/DesistDaydream/dtcg/internal/database"
 	"github.com/DesistDaydream/dtcg/pkg/dtcg/api/v1/models"
 	"github.com/gin-gonic/gin"
@@ -39,9 +41,24 @@ func PostCardsDesc(c *gin.Context) {
 	// 允许跨域
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 
-	var req models.PostCardsDescReq
+	// 绑定 url query
+	var reqQuery models.GetCardsDescReqQuery
 
-	if err := c.ShouldBindJSON(&req); err != nil {
+	if err := c.ShouldBindQuery(&reqQuery); err != nil {
+		logrus.Error(err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if reqQuery.PageSize == 0 || reqQuery.PageNum == 0 {
+		reqQuery.PageSize = 10
+		reqQuery.PageNum = 1
+	}
+
+	// 绑定请求体
+	var reqBody dbmodels.QueryCardDesc
+
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
 		logrus.Error(err)
 		c.AbortWithStatusJSON(http.StatusBadRequest, models.ReqBodyErrorResp{
 			Message: "请求体错误",
@@ -50,7 +67,7 @@ func PostCardsDesc(c *gin.Context) {
 		return
 	}
 
-	resp, err := database.GetCardDesc(req.PageSize, req.PageNum)
+	resp, err := database.GetCardDescByCondition(reqQuery.PageSize, reqQuery.PageNum, &reqBody)
 	if err != nil {
 		logrus.Errorf("%v", err)
 	}
