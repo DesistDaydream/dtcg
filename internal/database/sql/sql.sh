3a41e89404c337256de859f9bdea4ffbbd36e7c4
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-06-22 23:38:34 +0800

    Update sql.sh

--- internal/database/sql/sql.sh
+++ internal/database/sql/sql.sh
@@ -1,11 +1,12 @@
 # 有新包时，批量更新集换社的 card_version_id
+# 更新到 EXC-02
 #!/bin/bash
 
 read -p "enter password: " PASSWORD
 
-card_id=1579 # 数据库中的 ID，从这个 ID 的卡开始更新 card_version_id
-stop_id=1586 # 更新到该 ID 的卡为止
-cid=4601 # card_version_id 的值，从这个值开始更新
+card_id=1870 # 数据库中的 ID，从这个 ID 的卡开始更新 card_version_id
+stop_id=1967 # 更新到该 ID 的卡为止
+cid=4969 # card_version_id 的值，从这个值开始更新
 
 while [ $card_id -le $stop_id ]; do
   mysql -uroot -p"$PASSWORD" -e "UPDATE my_dtcg.card_prices SET card_version_id = $cid WHERE id = $card_id;"
