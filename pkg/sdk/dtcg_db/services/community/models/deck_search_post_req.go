c957fc0518795b62a2c9a268f9159e6a5ae3c052
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-07-18 22:51:53 +0800

    update

--- pkg/sdk/dtcg_db/services/community/models/deck_search_post_req.go
+++ pkg/sdk/dtcg_db/services/community/models/deck_search_post_req.go
@@ -6,7 +6,7 @@ type DeckSearchReqQuery struct {
 }
 
 type DeckSearchReqBody struct {
-	Tags  []string `json:"tags"`
-	Kw    string   `json:"kw"`
-	Envir string   `json:"envir"`
+	Envir string   `json:"envir"` // 环境。chs: 简中；ja: 日文
+	Kw    string   `json:"kw"`    // 搜索关键字
+	Tags  []string `json:"tags"`  // 共两个元素，分别是类别与颜色。3: 卡组分享；13: 国内比赛。5: 红色；不填: 无限制。
 }
