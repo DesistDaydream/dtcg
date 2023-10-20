42abd9a48cbadbfdface0e3d2a2bfe6e604e5560
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-10-20 15:08:42 +0800

    Update card_price.sql

--- internal/database/sql/card_price.sql
+++ internal/database/sql/card_price.sql
@@ -19,6 +19,18 @@ FROM
     card_prices
 WHERE set_prefix = 'SPC-1A';
 
+--- 获取多个卡包中的所有卡牌的价格的总和
+SELECT
+    set_prefix,
+    ROUND(SUM(min_price),2) AS all_min_price,
+    ROUND(SUM(avg_price),2) AS all_avg_price
+FROM
+    card_prices
+WHERE
+    set_prefix IN ('BTC-04','BTC-05', 'BTC-06', 'BTC-07')
+GROUP BY
+    set_prefix;
+
 --- 查询价格低于 500 的所有卡牌价格的总和
 SELECT
     SUM(if(min_price < 500, min_price, 0)) AS all_min_price,
